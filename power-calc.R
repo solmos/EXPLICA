10d9f6747fe50afc3aa011497cd72c68c7e9e5e5
Sergio Olmos <s.olmos.pardo@gmail.com>
2021-02-15 17:20:20 +0100

    Modified some parameters of the simulations

--- power-calc.R
+++ power-calc.R
@@ -38,41 +38,8 @@ sims_hs <- simPvalues(
 )
 saveRDS(sims_hs, "sims_hs.rds")
 
+## calcPower(sims_homocysteine) %>%
+##   plotPower()
 
 ## readRDS("sims.rds")
-
-sims <- sims_apob
-coefs <- map_df(sims, ~ .$coef)
-p_values <- map_df(sims, ~ .$p_values)
-
-
-power_df <- p_values %>%
-  group_by(n, max_effect, ) %>%
-  summarize(
-    power = sum(p.value < 0.05) / n(),
-    power_cat = case_when(
-      power < 0.5 ~ 1,
-      power >= 0.5 & power < 0.7 ~ 2,
-      power >= 0.7 & power < 0.8 ~ 3,
-      power > 0.8 ~ 4
-    )
-  )
-
-power_df %>%
-  ggplot(aes(max_effect, power, group = n, color = n)) +
-  geom_line(alpha = 0.7) +
-  geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey37") +
-  scale_y_continuous(breaks = seq(0, 1, by = 0.2)) +
-  scale_color_continuous(name = "Number of expotype categories") +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 11),
-    axis.title = element_text(size = 11),
-    legend.position = "bottom"
-  ) +
-  labs(
-    title = "Power for different number of expotype categories",
-    x = "Maximum difference",
-    y = "Power"
-  )
 
